6aaefc397f1ebcff59a6d9f4e077b484eaab77e2
Dian Octaviani <dian_octaviani03@hotmail.com>
2021-08-27 16:53:17 +1000

    re-structured directories

--- m1_selenium/selenium_img_src_crawler.py
+++ m1_selenium/selenium_img_src_crawler.py
@@ -3,20 +3,27 @@ from typing import ItemsView
 from pandas.core.frame import DataFrame
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
 
 
-keywords = pd.read_csv('scientific_botanical_names_veggies_fruits.csv')
-df = pd.DataFrame(columns=['search_terms','src_base64'])
+keywords = pd.read_csv('./dataset/scientific_botanical_names_veggies_fruits.csv')
+lnk_output = pd.DataFrame(columns=['search_terms','src_base64'])
 
 def search_google(search_query):
     browser = webdriver.Chrome(ChromeDriverManager().install())
     search_url = f"https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&q={search_query}"
-    images_url = []
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument("--disable-web-security")
+    options.add_argument("--allow-running-insecure-content")
+    options.add_argument("--allow-cross-origin-auth-prompt")
 
     # Open browser to begin search
     browser.get(search_url)
-    elements = browser.find_elements_by_class_name('rg_i')
 
     # XPath for the 1st image that appears in Google: //*[@id="islrg"]/div[1]/div[1]/a[1]/div[1]/img
     img_box = browser.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[1]/a[1]/div[1]/img')
@@ -37,9 +44,9 @@ def search_google(search_query):
 # Function to write the DataFrame of the image source links (Base 64)
 def write_df(a, b):
     row = [a, b]
-    df.loc[len(df)] = row
-    df.append(row, ignore_index=True)
-    df.to_csv("img_src_links.csv", index=False, mode='w', header=False, sep='|')
+    lnk_output.loc[len(lnk_output)] = row
+    lnk_output.append(row, ignore_index=True)
+    lnk_output.to_csv("m1_selenium/output/links/img_src_links.csv", index=False, mode='w', header=False, sep='|')
 
 
 for i in keywords['scientific_names']:
